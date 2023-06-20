ed519adaefe2915c58f22dffead67596401df610
Briuor <briuorlol89@gmail.com>
2023-06-20 15:37:46 -0300

    not exposing build folder anymore, and removing rimraf not necessary

--- build/utils/functions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stopExecution = void 0;
-function stopExecution(text) {
-    console.log(text);
-    process.exit();
-}
-exports.stopExecution = stopExecution;
